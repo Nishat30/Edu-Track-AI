7a7979260d757357aac179588376536231d40bef
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-09-25 14:50:14 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
   server: {
-    allowedHosts: [
-      'edu-track-ai.onrender.com', // 👈 add your domain here
-    ],
+    host: "::",
+    port: 8080,
   },
-})
+  plugins: [react()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
